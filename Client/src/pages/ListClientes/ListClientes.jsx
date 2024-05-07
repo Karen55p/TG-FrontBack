5ca591b3969bc601703f84e0b5960c28a85eb545
Karen Passarella <karenpassarella@hotmail.com>
2024-05-06 21:09:23 -0300

    integração de listas feita

--- Client/src/pages/ListClientes/ListClientes.jsx
+++ Client/src/pages/ListClientes/ListClientes.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import './ListClientesStyle.css'
 import { Link } from "react-router-dom";
 import { Table } from "react-bootstrap"
 
 const Listcl = () => {
+    const [clientes, setClientes] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8001/clientes')
+            .then(response => {
+                setClientes(response.data);
+                console.log(response.data)
+            })
+            .catch(error => {
+                console.error('Erro ao buscar clientes:', error);
+            });
+    }, []);
+
     return (
         <div className="pagina">
             <div className="titulo"><h1>Lista de Clientes</h1></div>
@@ -11,11 +25,26 @@ const Listcl = () => {
                     <Table className="tabelacl">
                         <tread>
                             <tr>
-                                <th>
-                                    test
-                                </th>
+                                <th>Id</th>
+                                <th>Nome</th>
+                                <th>Telefone</th>
+                                <th>CPF</th>
+                                <th>Endereco</th>
+                                <th>Observação</th>
                             </tr>
                         </tread>
+                        <tbody>
+                            {clientes.map(cliente => (
+                                <tr key={cliente.id}>
+                                    <td>{cliente.id}</td>
+                                    <td>{cliente.nome}</td>
+                                    <td>{cliente.telefone}</td>
+                                    <td>{cliente.cpf}</td>
+                                    <td>{cliente.endereco}</td>
+                                    <td>{cliente.obs}</td>
+                                </tr>
+                            ))}
+                        </tbody>
                     </Table>
                     <Link className="botaolistcl" to='/login'>Modificar</Link>
                     <Link className="botaolistcl" to='/login'>Cadastrar</Link>
--- Client/src/pages/ListUsuários/ListUsuários.jsx
+++ Client/src/pages/ListUsuários/ListUsuários.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import './ListUsuáriosStyle.css'
 import { Link } from "react-router-dom";
 import { Table } from "react-bootstrap"
 
 const ListUs = () => {
+    const [usuarios, setUsuarios] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:8001/user')
+            .then(response => {
+                setUsuarios(response.data);
+                console.log(response.data)
+            })
+            .catch(error => {
+                console.error('Erro ao buscar usuarios:', error);
+            });
+    }, []);
+
     return (
         <div className="pagina">
             <div className="titulo"><h1>Lista de Usuários</h1></div>
@@ -11,11 +25,24 @@ const ListUs = () => {
                     <Table className="tabelaus">
                         <tread>
                             <tr>
-                                <th>
-                                    test
-                                </th>
+                                <th>Id</th>
+                                <th>Nome de Usuário</th>
+                                <th>Email</th>
+                                <th>Nível</th>
+                                <th>Senha</th>
                             </tr>
                         </tread>
+                        <tbody>
+                            {usuarios.map(user => (
+                                <tr key={user.id}>
+                                    <td>{user.id}</td>
+                                    <td>{user.username}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.nivel}</td>
+                                    <td>{user.senha}</td>
+                                </tr>
+                            ))}
+                        </tbody>
                     </Table>
                         <Link className="blistus1" to='/'>Modificar</Link>
                         <Link className="blistus2" to='/'>Cadastrar</Link>
